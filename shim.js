430f9e9d4b42ffe6e9f8d3b18718c2e58401a8fc
Mark Barr <mark@hyperingenuity.com>
2020-01-03 10:52:02 -0500

    shim: optional shim added

---
+++ shim.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const editDistance = require('./editDistance');
+const metaphone = require('./metaphone');
+const tokenize = require('./tokenize');
+
+Object.defineProperty(String.prototype, '$editDistance', {
+  value (string) {
+    return editDistance(this, string);
+  },
+  enumerable: false,
+  configurable: true
+});
+
+Object.defineProperty(String.prototype, '$metaphone', {
+  value () {
+    return metaphone(this);
+  },
+  enumerable: false,
+  configurable: true
+});
+
+Object.defineProperty(String.prototype, '$tokenize', {
+  value (regExp) {
+    return tokenize(this, regExp);
+  },
+  enumerable: false,
+  configurable: true
+});
+
+Object.defineProperty(String.prototype, '$treebank', {
+  value () {
+    return tokenize.treebank(this);
+  },
+  enumerable: false,
+  configurable: true
+});
